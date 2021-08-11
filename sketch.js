7c07a695da69df14a7cd0d8d1bc2570cf6857c69
JoelFoo12345 <88266144+JoelFoo12345@users.noreply.github.com>
2021-08-11 15:52:42 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var btn_red, red_bg
+var btn_green, green_bg
+
+function setup() {
+  createCanvas(400,400);
+
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg)
+}
+
+function draw() 
+{
+  background(30);
+
+  function red_bg()
+  {
+    background("red");
+  }
+  function green_bg()
+  {
+    background("green");
+  }
+
+}
+
+
+
+
